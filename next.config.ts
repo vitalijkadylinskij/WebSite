0d92352090682ad98e00f616189c86705e4c3094
vitalijkadylinskij <vitalijkadylinskij@gmail.com>
2025-08-27 15:36:54 +0300

    delete output for next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {
-  output: "export",
-};
+const nextConfig: NextConfig = {};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
